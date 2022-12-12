ace11ecedfbe7aeeada7eaeb2d75a8f3387db039
Marcos Diaz <marcosreuquendiaz@gmail.com>
2022-12-12 10:03:19 -0300

    solve build problems

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
-import { Large, Title } from 'UI/text'
-import Form from "components/form/form"
-import css from "./index.module.css";
-import { Layout } from 'UI/layout';
-import { FormContainer } from './styled';
+import { Large, Title } from "UI/text";
+import Form from "components/form/form";
+import css from "../styles/index.module.css";
+import { Layout } from "UI/layout";
+import { FormContainer } from "../styles/styled";
 
 export default function Home() {
-  return <Layout>
-    <Title className={css["hero-title"]}>Mentees</Title>
-    <Large className={css["hero-text"]}>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Corporis sapiente libero atque ad labore ducimus quisquam nisi aliquam, 
-      excepturi, quidem suscipit. Debitis modi nisi autem totam 
-      obcaecati tempore, dolorem error.
-    </Large> 
-    <FormContainer>
-      <Form/>
-    </FormContainer>
-  </Layout>
+  return (
+    <Layout>
+      <Title className={css["hero-title"]}>Mentees</Title>
+      <Large className={css["hero-text"]}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+        sapiente libero atque ad labore ducimus quisquam nisi aliquam,
+        excepturi, quidem suscipit. Debitis modi nisi autem totam obcaecati
+        tempore, dolorem error.
+      </Large>
+      <FormContainer>
+        <Form />
+      </FormContainer>
+    </Layout>
+  );
 }
