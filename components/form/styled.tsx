7c9f11f6ffdc30ffd73cda4d31f6ff491e086cbc
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-12-13 09:49:37 -0300

    add button effects

--- components/form/styled.tsx
+++ components/form/styled.tsx
@@ -65,9 +65,10 @@ export const Textarea = styled.textarea`
   border: 1.5px solid var(--blanco);
   color: var(--blanco);
   text-align: center;
+  padding: 20px;
 `;
 
-export const Button = styled.input`
+export const Button = styled.button`
   font-size: 16px;
   color: var(--blanco);
   background-color: transparent;
@@ -78,6 +79,20 @@ export const Button = styled.input`
   display: block;
   margin: 20px auto 0 auto;
   font-weight: bold;
+  cursor: pointer;
+  user-select:none;
+  text-decoration: none;
+  transition-duration: 0.4s;
+  -webkit-transition-duration: 0.4s; 
+
+  :hover {
+  transition-duration: 0.1s;
+  background-color: #3A3A3A;
+}
+:active {
+            box-shadow:1px 6px 50px 30px rgba(190, 184, 184, 0.24);
+            transform: translateY(4px);
+        }
 `;
 
 export const AlertText = styled.p`
