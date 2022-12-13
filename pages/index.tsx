e694f8e798620e312e3762128bfdf8521343804a
Marcos Diaz <marcosreuquendiaz@gmail.com>
2022-12-13 13:08:58 -0300

    some ui fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,28 @@ import Form from "components/form/form";
 import css from "../styles/index.module.css";
 import { Layout } from "UI/layout";
 import { FormContainer } from "../styles/styled";
+import styled from "styled-components";
+
+const ViewHeight = styled.div`
+  min-height: 100vh;
+`;
 
 export default function Home() {
   return (
     <Layout>
-      <Title className={css["hero-title"]}>Mentees</Title>
-      <Large className={css["hero-text"]}>
-      En mentees somos un grupo que busca formar una comunidad con personas que quieran compartir conocimientos.
-      Si queres formar parte podes registrarte como mentor completando el formulario de inscripcion. 
-      Si lo que buscas es ayuda podes ponerte en contacto con alguno de nuestros mentores.
-      </Large>
-      <FormContainer>
-        <Form />
-      </FormContainer>
+      <ViewHeight>
+        <Title className={css["hero-title"]}>Mentees</Title>
+        <Large className={css["hero-text"]}>
+          En mentees somos un grupo que busca formar una comunidad con personas
+          que quieran compartir conocimientos. Si queres formar parte podes
+          registrarte como mentor completando el formulario de inscripcion. Si
+          lo que buscas es ayuda podes ponerte en contacto con alguno de
+          nuestros mentores.
+        </Large>
+        <FormContainer>
+          <Form />
+        </FormContainer>
+      </ViewHeight>
     </Layout>
   );
 }
